ddd663a52ceba26f07065c66c243e559739e4297
anazj <ahowell9070@gmail.com>
2024-01-31 12:43:37 -0500

    env variable now working - can add secret later

--- api/models/database.js
+++ api/models/database.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-//const uri = process.env.DB_URI;
+require('dotenv').config({path:'../.env'});
+const uri = process.env.DB_URI;
 
 const db = require('mongoose'); 
-
+//"mongodb+srv://Doc11:RD4LrYfBVyl7l5ij@docdb.o1oipv1.mongodb.net/DocDB?retryWrites=true&w=majority"
 //connecting to the database
-db.connect("mongodb+srv://Doc11:RD4LrYfBVyl7l5ij@docdb.o1oipv1.mongodb.net/DocDB?retryWrites=true&w=majority")
+db.connect(uri)
 .then((res)=>
 {
     console.log("successful connection to the database");
