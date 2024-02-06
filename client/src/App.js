b7a9a5329d9ec99315c5c140b827a0b04bc34e02
anazj <ahowell9070@gmail.com>
2024-02-06 01:32:47 -0500

    fixed merge conflict manually

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
-
-import { useApi } from './hooks/use-api';
+import Navbar from './components/Navbar/Navbar';
+import Table from './components/Table/Table';
 
 function App() {
-  const { response } = useApi();
+  const response = "Welcome to the COVID-19 Chest X-Ray Image Analysis Tool!";
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          {response}
-        </p>
+      <header className="App-header">        
+          <div>
+            {response}  
+          </div>
+        <div className='container'>
+          <Navbar/>
+        </div>
+          <div> 
+              <Table />
+          </div>
       </header>
     </div>
   );
