315af61c9bce0b859f3472e96e579b1347affffe
aj-howell <93935073+aj-howell@users.noreply.github.com>
2024-02-18 19:14:10 -0500

    Delete api/routes/authRoute.js

--- api/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController=require('../controllers/authorization_controller');
-
-router.post('/', AuthController.login);
-
-
-
-module.exports = router;
